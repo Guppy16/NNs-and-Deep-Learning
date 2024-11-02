6ed4b3b60267429f529d68ef628957ba5df0a4de
Akash Gupta <guptaalgebra@gmail.com>
2024-11-02 00:39:27 +0000

    feat: normalised mnist

--- experiments/data.py
+++ experiments/data.py
@@ -1,15 +1,14 @@
 """File for dataloaders and data processing functions."""
 
+from pathlib import Path
 from typing import TypeVar
-from experiments.base import RESOURCES, SEED, logger
 
 import torch
-from torchvision.datasets import MNIST, CIFAR10  # type: ignore
+from torch.utils.data import DataLoader, Dataset, Subset, random_split
+from torchvision.datasets import CIFAR10, MNIST, VisionDataset  # type: ignore
 from torchvision.transforms import v2  # type: ignore
-from torch.utils.data import DataLoader, random_split, Subset, Dataset
 
-
-from pathlib import Path
+from experiments.base import RESOURCES, SEED, logger
 
 ROOT_PATH = RESOURCES / "data"
 
@@ -40,8 +39,8 @@ def MNISTDataset(
 
 
 def train_val_dataset_split(
-    dataset: Dataset[T], val_size: int = 10000, seed: int = SEED
-) -> tuple[Subset[T], ...]:
+    dataset: VisionDataset, val_size: int = 10000, seed: int = SEED
+) -> tuple[Subset, ...]:
     """Return train - validation split given a torch dataset"""
     # Create a seeded rng to determinstically get a validation set
     rng = torch.Generator().manual_seed(seed)
@@ -94,8 +93,49 @@ def get_cifar(root_path: Path = ROOT_PATH, subset: int = 10):
     return cifar_trainset, cifar_valset, cifar_testset
 
 
+def get_normalised_mnist(subset: int = 1, train_val_split=[0.9, 0.1]):
+    """Return train, validation and test sets for normalised MNIST.
+
+    Normalisation: mean 0, std 1
+
+    Args:
+        subset: Get every `subset`-th element from the dataset.
+        Equivalently, the fraction of the dataset to use is 1 / `subset`.
+    """
+
+    # Create a seeded rng to determinstically get a validation set
+    rng = torch.Generator().manual_seed(SEED)
+
+    t = v2.Compose(
+        [
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True),  # scale to [0, 1]
+            v2.Normalize(mean=[0.1307], std=[0.3081]),
+        ]
+    )
+
+    root = str(ROOT_PATH)
+    train_data = MNIST(root=root, train=True, download=True, transform=t)
+    test_data = MNIST(root=root, train=False, download=True, transform=t)
+
+    logger.info("Train size: %s", len(train_data))
+    logger.info("Test size: %s", len(test_data))
+
+    if subset > 1:
+        train_data = Subset(train_data, indices=range(0, len(train_data), subset))
+        test_data = Subset(test_data, indices=range(0, len(test_data), subset))
+
+    train_data, val_data = random_split(train_data, train_val_split, generator=rng)
+
+    return train_data, val_data, test_data
+
+
 if __name__ == "__main__":
     train_set, test_set = MNISTDataset()
     train_set, val_set = train_val_dataset_split(train_set, val_size=10000)
 
     trainset, valset, testset = get_cifar()
+
+    trainset, valset, testset = get_normalised_mnist()
+    breakpoint()
+    print("Done")
