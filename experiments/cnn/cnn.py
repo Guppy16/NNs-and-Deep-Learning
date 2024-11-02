652decb35331dc2eceaea23479438b2bda813f85
Akash Gupta <guptaalgebra@gmail.com>
2024-11-02 00:39:36 +0000

    wip: cnn

--- experiments/cnn/cnn.py
+++ experiments/cnn/cnn.py
@@ -12,10 +12,29 @@ from experiments.base import BaseConfig, BaseModel
 
 @dataclass
 class CNNConfig(BaseConfig):
-    out_features: int = 16
+    in_channels: int = 1
+    img_size: int = 28  # 1 x 28 x 28 image
+    hidden_features: int = 16
     n_classes: int = 10
 
 
 class CNN(BaseModel):
+    """Lightweight CNN"""
+
     def __init__(self, config: CNNConfig):
-        """"""
+        super().__init__()
+        self.config = config
+
+        self.nnet = nn.Sequential(
+            nn.Conv2d(
+                self.config.in_channels,
+                self.config.hidden_features,
+                kernel_size=7,
+                stride=2,
+                padding=3,
+                bias=False,
+            ),
+            nn.BatchNorm2d(self.config.hidden_features),
+        )
+        # TODO: padding should be equal to the "zeros" on the border
+        # But this may not be zero anymore due to the normalisation
